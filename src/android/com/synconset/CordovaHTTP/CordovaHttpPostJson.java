180dc18327e238de0e5a91ec9c8db6197c2d9275
tigergroup <chris28@gmail.com>
2017-05-20 07:36:24 -0500

    u

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPostJson.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPostJson.java
@@ -17,19 +17,21 @@ import android.util.Log;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.github.kevinsawicki.http.HttpRequest.HttpRequestException;
  
-public class CordovaHttpPut extends CordovaHttp implements Runnable {
-    public CordovaHttpPut(String urlString, Map<?, ?> params, Map<String, String> headers, CallbackContext callbackContext) {
-        super(urlString, params, headers, callbackContext);
+public class CordovaHttpPostJson extends CordovaHttp implements Runnable {
+    
+    public CordovaHttpPostJson(String urlString, JSONObject jsonObj, Map<String, String> headers, CallbackContext callbackContext) {
+        super(urlString, jsonObj, headers, callbackContext);
     }
     
     @Override
     public void run() {
         try {
-            HttpRequest request = HttpRequest.put(this.getUrlString());
+            HttpRequest request = HttpRequest.post(this.getUrlString());
             this.setupSecurity(request);
-            request.acceptCharset(CHARSET);
             request.headers(this.getHeaders());
-            request.form(this.getParams());
+            request.acceptJson();
+            request.contentType(HttpRequest.CONTENT_TYPE_JSON);
+            request.send(getJsonObject().toString());
             int code = request.code();
             String body = request.body(CHARSET);
             JSONObject response = new JSONObject();
