e62b3ed89960e0c5bdfb18e8c4134708084e5991
tigergroup <chris28@gmail.com>
2018-09-10 10:54:57 -0500

    code update

--- src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
+++ src/android/com/synconset/CordovaHTTP/CordovaHttpPlugin.java
@@ -71,7 +71,7 @@ public class CordovaHttpPlugin extends CordovaPlugin {
             CordovaHttpPost post = new CordovaHttpPost(urlString, jsonObj, headersMap, callbackContext);
             cordova.getThreadPool().execute(post);
         } 
-		else if (action.equals("put")) {
+		/*else if (action.equals("put")) {
             String urlString = args.getString(0);
             JSONObject jsonObj = args.getJSONObject(1);
             JSONObject headers = args.getJSONObject(2);
@@ -84,7 +84,7 @@ public class CordovaHttpPlugin extends CordovaPlugin {
             JSONObject headers = args.getJSONObject(2);
             HashMap<String, String> headersMap = this.getStringMapFromJSONObject(headers);
             CordovaHttpDelete delete = new CordovaHttpDelete(urlString, jsonObj, headersMap, callbackContext);
-            cordova.getThreadPool().execute(delete);
+            cordova.getThreadPool().execute(delete);*/
 		}else if (action.equals("enableSSLPinning")) {
             try {
                 boolean enable = args.getBoolean(0);
